be829e56bfabe6c6955e26cf7c44bfa6eb7bc74a
John Henry <johnhmejia02@gmail.com>
2023-07-10 20:14:05 -0500

    f

---
+++ get_old_data.py
@@ -0,0 +1,35 @@
+# Stock trading app
+
+import os
+
+import pandas as pd
+
+# Get stock list from stock_info.csv
+stock_list = pd.read_csv('stock_info.csv')
+
+# Get ticker strings from stock_list
+tickerStrings = stock_list['Ticker'].tolist()
+
+import yfinance as yf
+
+for ticker in tickerStrings:
+    # Get data on this ticker
+    tickerData = yf.Ticker(ticker)
+
+    # Get today's date
+    today = pd.Timestamp.today()
+
+    # Get the date 3 months ago
+    three_months_ago = today - pd.DateOffset(months=3)
+
+
+    # Get the historical prices for this ticker
+    tickerDf = tickerData.history(period='1d', start=three_months_ago, end=today)
+
+    print(tickerDf)
+
+    # Save data to csv
+    tickerDf.to_csv(ticker + '.csv')
+
+
+
